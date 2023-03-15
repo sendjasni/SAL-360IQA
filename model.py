7683ed3d9e983456ac06297df2b7d45ad393e50b
asendjasni <sendjasni.abderrezzaq@gmail.com>
2023-03-15 16:19:23 +0100

    Model: callbacks function

--- model.py
+++ model.py
@@ -52,3 +52,72 @@ class Sal360Model():
 
         iqa_model = tf.keras.models.Model(inputs=inputs, outputs=output)
         return iqa_model
+
+    class EarlyStoppingAtMinLoss(tf.keras.callbacks.Callback):
+    """Stop training when the loss is at its min, i.e. the loss stops decreasing.
+
+  Arguments:
+      patience: Number of epochs to wait after min has been hit. After this
+      number of no improvement, training stops.
+  """
+
+    def __init__(self, patience=15):
+        super(EarlyStoppingAtMinLoss, self).__init__()
+        self.patience = patience
+        # best_weights to store the weights at which the minimum loss occurs.
+        self.best_weights = None
+
+    def on_train_begin(self, logs=None):
+        # The number of epoch it has waited when loss is no longer minimum.
+        self.wait = 0
+        # The epoch the training stops at.
+        self.stopped_epoch = 0
+        # Initialize the best as infinity.
+        self.best = np.Inf
+
+    def on_epoch_end(self, epoch, logs=None):
+        current = logs.get("loss")
+        if np.less(current, self.best):
+            self.best = current
+            self.wait = 0
+            # Record the best weights if current results is better (less).
+            self.best_weights = self.model.get_weights()
+        else:
+            self.wait += 1
+            if self.wait >= self.patience:
+                self.stopped_epoch = epoch
+                self.model.stop_training = True
+                print("Restoring model weights from the end of the best epoch.")
+                self.model.set_weights(self.best_weights)
+
+    def on_train_end(self, logs=None):
+        if self.stopped_epoch > 0:
+            print("Epoch %05d: early stopping" % (self.stopped_epoch + 1))
+
+
+def create_callbacks_fun(fold_no, path, bs, version):
+
+    log_file = path + '/Losses_fold_' + str(fold_no) + \
+        '_BS_' + str(bs) + '_version_' + version + '.log'
+
+    csv_loger = tf.keras.callbacks.CSVLogger(
+        log_file, separator=",", append=False)
+
+    filepath = path + '/Weights_fold_' + str(fold_no) + \
+        '_BS_' + str(bs) + '_version_' + version + '.hdf5'
+
+    checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath,
+                                                    monitor="val_loss",
+                                                    mode="min",
+                                                    save_best_only=True,
+                                                    save_weights_only=True,
+                                                    save_freq='epoch',
+                                                    verbose=1)
+
+    callbacks = [
+        EarlyStoppingAtMinLoss(),
+        csv_loger,
+        checkpoint,
+    ]
+
+    return callbacks
